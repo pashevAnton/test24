89fb16808d9f8f80dc09148154e5d5324ea909be
PagilayaSkumbria54 <toni.pashev@bk.ru>
2023-07-24 01:52:10 +0300

    Add widget in next project and change TimeDisplayColor

--- next/src/components/CurrentTimeDisplayWithWidget/CurrentTimeDisplayWithWidget.tsx
+++ next/src/components/CurrentTimeDisplayWithWidget/CurrentTimeDisplayWithWidget.tsx
@@ -1,32 +1,32 @@
 'use client'
 
-import React, { FC, useEffect, useState } from "react";
+import React, {FC, useEffect, useState} from "react";
 import "./CurrentTimeDisplayWithWidget.css";
+import { useWidget } from "@/hooks/useWidget";
 
 const CurrentTimeDisplayWithWidget: FC = () => {
-  const [currentTime, setCurrentTime] = useState<Date | null>(null);
+    const [currentTime, setCurrentTime] = useState<Date | null>(null);
+    useWidget()
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+    return (
+        <div className="current-time-display-with-widget" data-time={currentTime?.toString()}>
+            <div className="current-time-display-with-widget__time">
+                {currentTime?.toString()}
+            </div>
+            <div className="widget-container exampleWrapperContent">
 
-  return (
-    <div
-      className="current-time-display-with-widget"
-      data-time={currentTime?.toString()}
-    >
-      <div className="current-time-display-with-widget__time">
-        {currentTime?.toString()}
-      </div>
-      {/*  PUT_WIDGET_HERE */}
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
 
-export { CurrentTimeDisplayWithWidget };
+export {CurrentTimeDisplayWithWidget};
